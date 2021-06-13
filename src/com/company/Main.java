ff6ae561edae04a2509c861b085b61ca79d54c54
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-06-13 12:39:59 +0200

    Commit D

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Commit A");
         System.out.println("Commit B");
         System.out.println("Commit C");
+        System.out.println("Commit D");
     }
 }
